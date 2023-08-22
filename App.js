093b0b86fb64a0e2f0d5ed0857dac968f6a79741
RoushanFW196 <roushankumar6338@gmail.com>
2023-08-22 23:04:58 +0530

    updated the code

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi Nandani, I love you</Text>
+      <Text>very very much.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },
+});
